6a054154b61fe108c6dea7c71d96c12142d2d7ae
FFerreira92 <F92ferreira@gmail.com>
2022-07-13 00:43:36 +0100

    Added Lessee entity

---
+++ MyLeasing.Web/Data/Entities/Lessee.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Lessee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Document*")]
+        public int Document { get; set; }
+
+        [Required]
+        [Display(Name = "First Name*")]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "Last Name*")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public long? FixedPhone { get; set; }
+
+
+        [Display(Name = "Cell Phone")]
+        public long? CellPhone { get; set; }
+
+
+        public string Address { get; set; }
+
+        public string Photo { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
