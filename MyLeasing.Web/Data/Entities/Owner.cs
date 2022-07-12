68406df78512a017e8931805db6926fa3a38713d
FFerreira92 <F92ferreira@gmail.com>
2022-07-12 19:26:32 +0100

    Added User to OwnersAPI and fixed OwnersAPI Image Path

--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -33,6 +33,19 @@ namespace MyLeasing.Web.Data.Entities
         [Display(Name = "Image")] 
         public string ImageUrl { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://localhost:44368{ImageUrl.Substring(1)}";
+            }
+        }
+
         public User User { get; set; } 
     }
 }
