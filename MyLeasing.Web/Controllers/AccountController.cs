66a39b97ef683206a8293101c3007f993c5d9511
FFerreira92 <F92ferreira@gmail.com>
2022-07-19 16:26:55 +0100

    Register New Users

--- MyLeasing.Web/Controllers/AccountController.cs
+++ MyLeasing.Web/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MyLeasing.Web.Data.Entities;
 using MyLeasing.Web.Helpers;
 using MyLeasing.Web.Models;
 
@@ -9,10 +12,11 @@ namespace MyLeasing.Web.Controllers
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
-
+        private Random _random;
         public AccountController(IUserHelper userHelper)
         {
             _userHelper = userHelper;
+            _random = new Random();
         }
 
         public IActionResult Login()
@@ -54,5 +58,63 @@ namespace MyLeasing.Web.Controllers
             await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterNewUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(model.Username);
+
+                if (user == null)
+                {
+                    user = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Username,
+                        UserName = model.Username,
+                        Document = _random.Next(9999999).ToString()
+
+                    };
+
+                    var result = await _userHelper.AddUserAsync(user, model.Password);
+
+                    if (result != IdentityResult.Success)
+                    {
+                        ModelState.AddModelError(string.Empty, "The user couldn't be created.");
+                        return View(model);
+                    }
+
+                    var loginViewModel = new LoginViewModel
+                    {
+                        Password = model.Password,
+                        RememberMe = false,
+                        UserName = model.Username
+                    };
+
+                    var result2 = await _userHelper.LoginAsync(loginViewModel);
+
+                    if (result2.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    ModelState.AddModelError(string.Empty, "The user couldn't be logged.");
+                }
+            }
+
+            return View(model);
+        }
+
     }
+
+
+
 }
